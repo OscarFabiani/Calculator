035c4a6b4ff5ea2189c20115c3989acf150fcc4a
OscarFabiani <spartan539@yahoo.com>
2019-03-02 16:10:03 -0800

    Finished styling, added button press styling, and added function to add event listeners to elements.

--- script.js
+++ script.js
@@ -1,206 +1,3 @@
-/*
-function inputNumber() {
-  //THE LOGIC FOR THIS FUNCTION LOOKS FUNKY. I THINK I CAN CLEAN IT UP. IT ALSO MAY BE A GOOD IDEA TO SEPERATE THE ZERO BUTTON'S LOGIC FROM THE 1-9 LOGIC.
-  if (output.textContent.length > 20) {
-    console.log('number:1');
-    output.textContent = 'DIGIT LIMIT MET';
-  }
-  else if (input.textContent.length > 29) {
-    console.log('number:2');
-    output.textContent = 'SCREEN LIMIT MET';
-  }
-  else if (output.textContent == 'DIGIT LIMIT MET') {
-    console.log('number:3');
-  }
-  else if (/\=/.test(input.textContent)) {
-    console.log('number:4');
-    input.textContent = this.textContent;
-    output.textContent = this.textContent;
-  }
-  else if(/\./.test(output.textContent) && this.textContent == '.') {
-    console.log('number:5');
-  }
-  else if (/(?<![0-9])0$/.test(input.textContent) && this.textContent == '.' && output.textContent == '0') {
-    input.textContent += this.textContent;
-    output.textContent += this.textContent;
-    console.log('number:6');
-  }
-  else if (/(?<![0-9])0$/.test(input.textContent) && this.textContent == '.') {
-    input.textContent = input.textContent.slice(0, -1);
-    output.textContent = output.textContent.slice(0, -1);
-    output.textContent += this.textContent;
-    input.textContent += this.textContent;
-    console.log('number:7');
-  }
-  else if (/(?<![0-9])0$/.test(input.textContent) && output.textContent == '0') {
-    input.textContent = input.textContent.slice(0, -1);
-    output.textContent = output.textContent.slice(0, -1);
-    input.textContent += this.textContent;
-    output.textContent += this.textContent;
-    console.log('number:8');
-  }
-  //This if checks is output is 0 and this's textcontent is 0 then does nothing else but sets or "leaves" output 0.
-  else if (output.textContent == '0' && this.textContent == '0') {
-    output.textContent = '0';
-    console.log('number:9');
-  }
-  else if (output.textContent == '0' && this.textContent =='.' && input.textContent == '') {
-    output.textContent = '0.';
-    input.textContent += '0.';
-    console.log('number:10');
-  }
-  else if (output.textContent == '0' && this.textContent == '.') {
-    output.textContent = '0.';
-    input.textContent += this.textContent;
-    console.log('number:11');
-  }
-  //This effectively checks if output is 0 and this's textContent is 1-9 then sets output to nothing before appending this.textContent to output and input.
-  else if (output.textContent == '0') {
-    output.textContent = output.textContent.slice(1);
-    output.textContent += this.textContent;
-    input.textContent += this.textContent;
-    console.log('number:12');
-  }
-  else if(isNaN(output.textContent) && this.textContent == '.') {
-    output.textContent = '0.';
-    input.textContent += '0.';
-    console.log('number:13');
-  }
-  //This checks if output is NaN(which it would be after an operator is clicked) then sets output to this and appends this to input.
-  else if(isNaN(output.textContent)) {
-    output.textContent = this.textContent;
-    input.textContent += this.textContent;
-    console.log('number:14');
-  }
-  else {
-    console.log('number:15');
-    output.textContent += this.textContent;
-    input.textContent += this.textContent;
-  }
-}
-
-function inputNumber() {
-  //This is a function that dictates what happens when a number(0-9) is input.
-
-  if (output.textContent.length > 20) {
-    console.log('number:1');
-    output.textContent = 'DIGIT LIMIT MET';
-  }
-  else if (input.textContent.length > 29) {
-    console.log('number:2');
-    output.textContent = 'SCREEN LIMIT MET';
-  }
-  else if (/\=/.test(input.textContent)) {
-    console.log('number:4');
-    output.textContent = this.textContent;
-    input.textContent = this.textContent;
-  }
-  //checks if input ends with a 0 and is preceded by a '+', '-', '*', '/', or '.'(+0, -0, /0, *0).
-  else if (/(?<=[\+\-\/\*])0$/.test(input.textContent)) {
-    input.textContent = input.textContent.slice(0, -1);
-    output.textContent = this.textContent;
-    input.textContent += this.textContent;
-    console.log('number:8');
-  }
-  //checks if input is empty and this is '0'.
-  else if (!input.textContent && this.textContent == '0') {
-    console.log('number:9');
-  }
-  //checks if input is empty OR ends with a '+', '*', or '/' OR ends with a '-' that is preceded by a number.
-  else if (/^$|[\+\*\/]$|(?<=[0-9]-$)/.test(input.textContent)) {
-    output.textContent = this.textContent;
-    input.textContent += this.textContent;
-    console.log('number:13');
-  }
-  else if(!/T/.test(output.textContent)) {
-    console.log('number:15');
-    output.textContent += this.textContent;
-    input.textContent += this.textContent;
-  }
-}
-
-function inputOperator() {
-  //This is a function that dictates what happens when an operator(+, *, /) is input.
-  
-  if (/\=/.test(input.textContent)) {
-    console.log('operator:1');
-    input.textContent = input.textContent.slice(1);
-    input.textContent += this.textContent;
-    output.textContent = this.textContent;
-  }
-  //checks if input ends with a - and is preceded by a '*' or '/'.
-  else if (/(?<=[\*\/])-$/.test(input.textContent)) {
-    console.log('operator:3');
-    input.textContent = input.textContent.slice(0, -2);
-    input.textContent += this.textContent;
-    output.textContent = this.textContent;
-  }
-  //checks if the last character of input is a '+', '*', '/', or '.' OR if the last character of input is a '-' preceded by any character.
-  else if (/[\+\*\/\.]$|(?<=.)-$/.test(input.textContent)) {
-    console.log('operator:4');
-    input.textContent = input.textContent.slice(0, -1);
-    input.textContent += this.textContent;
-    output.textContent = this.textContent;
-  }
-  else if (!/^$|^-$/.test(input.textContent)) {
-    console.log('operator:5');
-    input.textContent += this.textContent;
-    output.textContent = this.textContent;
-  }
-}
-
-
-function inputMinus() {
-  //This is a function that dictates what happens when an subtract operator(-) is input.
-
-  if (/\=/.test(input.textContent)) {
-    console.log('minus:1')
-    input.textContent = input.textContent.slice(1);
-    input.textContent += this.textContent;
-    output.textContent = this.textContent;
-  }
-  //checks if input ends with a '+', '-', or '.'.
-  else if (/[\+\-\.]$/.test(input.textContent)) {
-    console.log('minus:2')
-    input.textContent = input.textContent.slice(0, -1);
-    input.textContent += this.textContent;
-    output.textContent = this.textContent;
-  }
-  else {
-    console.log('minus:3')
-    input.textContent += this.textContent;
-    output.textContent = this.textContent;
-  }
-}
-
-
-function calculate() {
-  //This is a function that dictates what happens when an equals operator(=) is input.
-
-  //checks if input ends with a '-' that is preceded by a '+', '*', '/', or '.'.
-  if(/(?<=[\+\*\/\.])-$/.test(input.textContent)) {
-    console.log('calculate:2');
-    input.textContent = input.textContent.slice(0, -2);
-    output.textContent = eval(input.textContent);
-    input.textContent = '=' + eval(input.textContent);
-  }
-  //checks if the last character of input is a '+', '*', '/', or '.' OR if the last character of input is a '-' preceded by any character.
-  else if(/[\+\*\/\.]$|(?<=.)-$/.test(input.textContent)) {
-    console.log('calculate:3');
-    input.textContent = input.textContent.slice(0, -1);
-    output.textContent = eval(input.textContent);
-    input.textContent = '=' + eval(input.textContent);
-  }
-  //checks if input is empty OR is '-' OR contains a '='.
-  else if(!/=|^-$|^$/.test(input.textContent)) {
-    console.log('calculate:4');
-    output.textContent = eval(input.textContent);
-    input.textContent = '=' + eval(input.textContent);
-  }
-}
-
-
-*/
 
 //Setting variables
 const input = document.getElementById('input');
@@ -223,39 +20,60 @@ const divide = document.getElementById('divide');
 const decimal = document.getElementById('decimal');
 const equals = document.getElementById('equals');
 
+const buttonElements = [clear, zero, one, two, three, four, five, six, seven, eight, nine, add, subtract, multiply, divide, decimal, equals];
+const numberElements = [zero, one, two, three, four, five, six, seven, eight, nine];
+
 
 //Adding event listeners
-clear.addEventListener('click', clearDisplay);
-zero.addEventListener('click', inputNumber);
-one.addEventListener('click', inputNumber);
-two.addEventListener('click', inputNumber);
-three.addEventListener('click', inputNumber);
-four.addEventListener('click', inputNumber);
-five.addEventListener('click', inputNumber);
-six.addEventListener('click', inputNumber);
-seven.addEventListener('click', inputNumber);
-eight.addEventListener('click', inputNumber);
-nine.addEventListener('click', inputNumber);
-decimal.addEventListener('click', inputDecimal);
-add.addEventListener('click', inputOperator);
-subtract.addEventListener('click', inputMinus);
-multiply.addEventListener('click', inputOperator);
-divide.addEventListener('click', inputOperator);
-equals.addEventListener('click', calculate);
+addButtonListeners(buttonElements);
+addNumberListeners(numberElements);
+clear.addEventListener('mousedown', clearDisplay);
+decimal.addEventListener('mousedown', inputDecimal);
+add.addEventListener('mousedown', inputOperator);
+subtract.addEventListener('mousedown', inputMinus);
+multiply.addEventListener('mousedown', inputOperator);
+divide.addEventListener('mousedown', inputOperator);
+equals.addEventListener('mousedown', calculate);
 
 
 //Initialize
 clearDisplay();
 
+
 //Functions
 
+function addButtonListeners(buttonElementList) {
+  buttonElementList.forEach((buttonElement) => {
+    buttonElement.addEventListener('mousedown', press);
+    buttonElement.addEventListener('mouseup', depress);
+    buttonElement.addEventListener('mouseout', depress);
+  })
+}
+
+function addNumberListeners(numberElementList) {
+  numberElementList.forEach((numberElement) => {
+    numberElement.addEventListener('mousedown', inputNumber);
+  })
+}
+
+function press() {
+  //Adds the 'pressed' class to the classList of an element.
+  this.classList.add('pressed');
+}
+
+function depress() {
+  //Removes the 'pressed' class to the classList of an element.
+  this.classList.remove('pressed')
+}
+
 function clearDisplay() {
+  //Resets the display.
   input.textContent = '';
   output.textContent = '0';
 }
 
 function inputNumber() {
-  //This is a function that dictates what happens when a number(0-9) is input.
+  //Dictates what happens when a number(0-9) is input.
 
   if (output.textContent.length > 20) {
     console.log('number:1');
@@ -268,7 +86,7 @@ function inputNumber() {
   else if (!input.textContent && this.textContent == '0') {//can I eliminate this "stopper"?
     console.log('number:9');
   }
-  else { //this can be unnested
+  else {
     if (/\=/.test(input.textContent)) {
       console.log('number:4');
       input.textContent = '';
@@ -294,7 +112,7 @@ function inputNumber() {
 }
 
 function inputDecimal() {
-  //This is a function that dictates what happens when a decimal(.) is input.
+  //Dictates what happens when a decimal(.) is input.
 
   if (input.textContent.length > 29) {
     console.log('number:2a');
@@ -325,7 +143,7 @@ function inputDecimal() {
 }
 
 function inputOperator() {
-  //This is a function that dictates what happens when an operator(+, *, /) is input.
+  //Dictates what happens when an operator(+, *, /) is input.
   
   if (/\=/.test(input.textContent)) {
     console.log('operator:1');
@@ -349,7 +167,7 @@ function inputOperator() {
 }
 
 function inputMinus() {
-  //This is a function that dictates what happens when an subtract operator(-) is input.
+  //Dictates what happens when an subtract operator(-) is input.
 
   if (/\=/.test(input.textContent)) {
     console.log('minus:1')
@@ -366,7 +184,7 @@ function inputMinus() {
 }
 
 function calculate() {
-  //This is a function that dictates what happens when an equals operator(=) is input.
+  //Dictates what happens when an equals operator(=) is input.
 
   //checks if input ends with a '-' that is preceded by a '+', '*', '/', or '.'.
   if(/(?<=[\+\*\/\.])-$/.test(input.textContent)) {
@@ -388,11 +206,10 @@ function calculate() {
 
  
 
-//FINISH STYLING
-
-//REVISIT INPUTNUMBER FUNCTION PERIODICALLY FOR POSSIBLE REFACTORING
+//REVISIT INPUTNUMBER FUNCTION PERIODICALLY FOR POSSIBLE REFACTORING.
 
 //NOTE: AS OF 2/26 TESTS ARE FAILING DUE TO FCC EXPECTING MY "OUTPUT" ELEMENT TO BE NAMED DISPLAY. A QUICK FIX WAS TO CHANGE OUTPUT TO DISPLAY AND DISPLAY TO "SCREEN"
 //WITHIN MY CODEPEN CODE BUT I'M NOT YET SURE IF THAT WILL BE MY PREFERRED SOLUTION
 
-//FEATURE: WHENEVER A/MOST BUTTONS ARE PRESSED AND NOTHING HAPPENS THE BUTTON FLASHES RED
+//FEATURE: WHENEVER A/MOST BUTTONS ARE PRESSED AND NOTHING HAPPENS(DISPLAY IS NOT UPDATED) THE BUTTON FLASHES RED.
+//FEATURE: ON/OFF BUTTON WHICH CLEARS/DIMS THE DISPLAY AND DISABLES BUTTON LIGHTING(LIKELY PLACED BETWEEN DISPLAY AND BUTTONS).
