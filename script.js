78d5721c9ba4920e69389e0c23528afccadd22a7
OscarFabiani <spartan539@yahoo.com>
2019-03-04 14:29:57 -0800

    Added red light on invalid button press feature and replaced mouse events with pointer events.

--- script.js
+++ script.js
@@ -27,13 +27,14 @@ const numberElements = [zero, one, two, three, four, five, six, seven, eight, ni
 //Adding event listeners
 addButtonListeners(buttonElements);
 addNumberListeners(numberElements);
-clear.addEventListener('mousedown', clearDisplay);
-decimal.addEventListener('mousedown', inputDecimal);
-add.addEventListener('mousedown', inputOperator);
-subtract.addEventListener('mousedown', inputMinus);
-multiply.addEventListener('mousedown', inputOperator);
-divide.addEventListener('mousedown', inputOperator);
-equals.addEventListener('mousedown', calculate);
+clear.addEventListener('pointerdown', clearDisplay);
+clear.addEventListener('pointerdown', press);
+decimal.addEventListener('pointerdown', inputDecimal);
+add.addEventListener('pointerdown', inputOperator);
+subtract.addEventListener('pointerdown', inputMinus);
+multiply.addEventListener('pointerdown', inputOperator);
+divide.addEventListener('pointerdown', inputOperator);
+equals.addEventListener('pointerdown', calculate);
 
 
 //Initialize
@@ -44,15 +45,16 @@ clearDisplay();
 
 function addButtonListeners(buttonElementList) {
   buttonElementList.forEach((buttonElement) => {
-    buttonElement.addEventListener('mousedown', press);
-    buttonElement.addEventListener('mouseup', depress);
+    buttonElement.addEventListener('pointerdown', press);
+    buttonElement.addEventListener('pointerup', depress);
     buttonElement.addEventListener('mouseout', depress);
+    buttonElement.addEventListener('pointerout', depress);
   })
 }
 
 function addNumberListeners(numberElementList) {
   numberElementList.forEach((numberElement) => {
-    numberElement.addEventListener('mousedown', inputNumber);
+    numberElement.addEventListener('pointerdown', inputNumber);
   })
 }
 
@@ -64,6 +66,7 @@ function press() {
 function depress() {
   //Removes the 'pressed' class to the classList of an element.
   this.classList.remove('pressed')
+  this.classList.remove('pressed2')
 }
 
 function clearDisplay() {
@@ -74,6 +77,9 @@ function clearDisplay() {
 
 function inputNumber() {
   //Dictates what happens when a number(0-9) is input.
+
+  let inputTracker = input.textContent;
+  let outputTracker = output.textContent;
 
   if (output.textContent.length > 20) {
     console.log('number:1');
@@ -109,10 +115,19 @@ function inputNumber() {
       input.textContent += this.textContent;
     }
   }
+  if (outputTracker == output.textContent && inputTracker == input.textContent) {
+    this.classList.add('pressed2');
+  }
+  else {
+    this.classList.add('pressed');
+  }
 }
 
 function inputDecimal() {
   //Dictates what happens when a decimal(.) is input.
+
+  let inputTracker = input.textContent;
+  let outputTracker = output.textContent;
 
   if (input.textContent.length > 29) {
     console.log('number:2a');
@@ -140,10 +155,20 @@ function inputDecimal() {
     input.textContent += this.textContent;
     output.textContent += this.textContent;
   }
+
+  if (outputTracker == output.textContent && inputTracker == input.textContent) {
+    this.classList.add('pressed2');
+  }
+  else {
+    this.classList.add('pressed');
+  }
 }
 
 function inputOperator() {
   //Dictates what happens when an operator(+, *, /) is input.
+
+  let inputTracker = input.textContent;
+  let outputTracker = output.textContent;
   
   if (/\=/.test(input.textContent)) {
     console.log('operator:1');
@@ -164,10 +189,52 @@ function inputOperator() {
     input.textContent += this.textContent;
     output.textContent = this.textContent;
   }
+
+  if (outputTracker == output.textContent && inputTracker == input.textContent) {
+    this.classList.add('pressed2');
+  }
+  else {
+    this.classList.add('pressed');
+  }
+}
+
+function inputOperator() {
+  //Dictates what happens when an operator(+, *, /) is input.
+  let inputTracker = input.textContent;
+  let outputTracker = output.textContent;
+  
+  if (/\=/.test(input.textContent)) {
+    console.log('operator:1');
+    input.textContent = input.textContent.slice(1);
+  }
+  //checks if input ends with a - and is preceded by a '*' or '/'.
+  else if (/(?<=[\*\/])-$/.test(input.textContent)) {
+    console.log('operator:3');
+    input.textContent = input.textContent.slice(0, -2);
+  }
+  //checks if the last character of input is a '+', '*', '/', or '.' OR if the last character of input is a '-' preceded by any character.
+  else if (/[\+\*\/\.]$|(?<=.)-$/.test(input.textContent)) {
+    console.log('operator:4');
+    input.textContent = input.textContent.slice(0, -1);
+  }
+  if (!/^$|^-$/.test(input.textContent)) {
+    console.log('operator:5');
+    input.textContent += this.textContent;
+    output.textContent = this.textContent;
+  }
+  if (outputTracker == output.textContent && inputTracker == input.textContent) {
+    this.classList.add('pressed2');
+  }
+  else {
+    this.classList.add('pressed');
+  }
 }
 
 function inputMinus() {
   //Dictates what happens when an subtract operator(-) is input.
+
+  let inputTracker = input.textContent;
+  let outputTracker = output.textContent;
 
   if (/\=/.test(input.textContent)) {
     console.log('minus:1')
@@ -181,10 +248,20 @@ function inputMinus() {
   console.log('minus:3')
   input.textContent += this.textContent;
   output.textContent = this.textContent;
+
+  if (outputTracker == output.textContent && inputTracker == input.textContent) {
+    this.classList.add('pressed2');
+  }
+  else {
+    this.classList.add('pressed');
+  }
 }
 
 function calculate() {
   //Dictates what happens when an equals operator(=) is input.
+
+  let inputTracker = input.textContent;
+  let outputTracker = output.textContent;
 
   //checks if input ends with a '-' that is preceded by a '+', '*', '/', or '.'.
   if(/(?<=[\+\*\/\.])-$/.test(input.textContent)) {
@@ -202,14 +279,22 @@ function calculate() {
     output.textContent = eval(input.textContent);
     input.textContent = '=' + eval(input.textContent);
   }
+
+  if (outputTracker == output.textContent && inputTracker == input.textContent) {
+    this.classList.add('pressed2');
+  }
+  else {
+    this.classList.add('pressed');
+  }
 }
 
  
+//REFACTOR RED LIGHT CODE
 
+//ADD CROSS=BROWSER COMPATABILITY
 //REVISIT INPUTNUMBER FUNCTION PERIODICALLY FOR POSSIBLE REFACTORING.
 
 //NOTE: AS OF 2/26 TESTS ARE FAILING DUE TO FCC EXPECTING MY "OUTPUT" ELEMENT TO BE NAMED DISPLAY. A QUICK FIX WAS TO CHANGE OUTPUT TO DISPLAY AND DISPLAY TO "SCREEN"
 //WITHIN MY CODEPEN CODE BUT I'M NOT YET SURE IF THAT WILL BE MY PREFERRED SOLUTION
 
-//FEATURE: WHENEVER A/MOST BUTTONS ARE PRESSED AND NOTHING HAPPENS(DISPLAY IS NOT UPDATED) THE BUTTON FLASHES RED.
 //FEATURE: ON/OFF BUTTON WHICH CLEARS/DIMS THE DISPLAY AND DISABLES BUTTON LIGHTING(LIKELY PLACED BETWEEN DISPLAY AND BUTTONS).
